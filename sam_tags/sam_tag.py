d818ff24f5ef9948c5c07ebfd01b4b1ffc49355b
Matt Stone <matthew.stone12@gmail.com>
2024-04-11 12:52:23 -0400

    wip: add sam_tag decorator

---
+++ sam_tags/sam_tag.py
@@ -0,0 +1,42 @@
+import re
+from enum import Enum
+from enum import unique
+from typing import TypeVar
+
+EnumerationT = TypeVar("EnumerationT", bound=type[Enum])
+"""
+An Enumeration type.
+
+Copied from `mypy`: https://github.com/python/mypy/blob/e2fc1f28935806ca04b18fab277217f583b51594/mypy/typeshed/stdlib/enum.pyi#L40
+"""
+
+TAG_REGEX = re.compile(r"^[A-Za-z][A-Za-z0-9]$")
+
+
+def sam_tag(enumeration: EnumerationT) -> type[Enum]:
+    """
+    Declare a locally-defined group of SAM tags.
+
+    This decorator enforces the following conventions on the decorated enum:
+
+    1. SAM tags must be unique.
+    2. SAM tags must not be a predefined standard tag.
+    3. SAM tags must be two-character strings.
+    4. Locally-defined tags must adhere to SAM convention, namely that tags start with "X", "Y", or
+       "Z", or are lowercase.
+    5. The enumeration class must inherit from `StrEnum` or `str`.
+    """
+
+    # TODO: accumulate errors
+
+    if not issubclass(enumeration, Enum):
+        raise TypeError("The `sam_tag` decorator may only be applied to `Enum` classes.")
+
+    if not issubclass(enumeration, str):
+        raise TypeError("SAM tags should inherit from `StrEnum` or mix in `str`.")
+
+    for tag in enumeration:
+        if not TAG_REGEX.match(tag.value):
+            raise ValueError(f"SAM tags must be two-character alphanumeric strings: {tag}")
+
+    return unique(enumeration)
---
+++ tests/test_sam_tag.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from enum import StrEnum
+
+import pytest
+
+from sam_tags import sam_tag
+
+
+def test_sam_tag() -> None:
+    """Test that we can declare a custom SAM tag."""
+
+    @sam_tag
+    class CustomTag(StrEnum):
+        XG = "XG"
+
+
+def test_sam_tag_raises_if_not_str() -> None:
+    """
+    Test that we raise a TypeError if the decorated class is not a `StrEnum` or
+    `str` mixin.
+    """
+
+    with pytest.raises(
+        TypeError,
+        match="SAM tags should inherit from `StrEnum` or mix in `str`.",
+    ):
+
+        @sam_tag
+        class BadTag(Enum):
+            XG = "XG"
+
+
+def test_sam_tag_raises_if_not_enum() -> None:
+    """
+    Test that we raise a TypeError if the decorated class is not an `Enum`.
+    """
+
+    with pytest.raises(
+        TypeError,
+        match="The `sam_tag` decorator may only be applied to `Enum` classes.",
+    ):
+        # NB: mypy (accurately) flags that `sam_tag` requires an enumeration
+        # type, but we want to test that this is also enforced at runtime for
+        # contexts where the user is not running a type checker.
+        @sam_tag
+        class BadTag(str):  # type: ignore[type-var]
+            XG = "XG"
+
+
+@pytest.mark.parametrize(
+    "tag",
+    [
+        "",  # empty string
+        "a",  # 1 character
+        "abc",  # 3 character
+        "1a",  # starts with number
+    ],
+)
+def test_sam_tag_raises_if_tags_are_not_two_characters(tag: str) -> None:
+    """
+    Test that we raise a ValueError if any of the enumeration's values are not
+    two-character strings.
+    """
+
+    with pytest.raises(
+        ValueError, match=f"SAM tags must be two-character alphanumeric strings: {tag}"
+    ):
+
+        @sam_tag
+        class BadTag(StrEnum):
+            XB = tag
